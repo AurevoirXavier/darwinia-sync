6242012d3ea8329eae6349a9e0b32496d3cdcc47
Xavier Lau <c.estlavie@icloud.com>
2020-05-19 12:21:28 +0800

    update: show system usage in linux

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@ mod global {
 
 	lazy_static! {
 		pub static ref darwinia_sync_pid: PID = process::id() as _;
-		pub static ref best_number: Regex = Regex::new(r".+?best.+?#(\d+)").unwrap();
+		pub static ref best_number_regex: Regex = Regex::new(r".+?best.+?#(\d+)").unwrap();
 	}
 }
 
 // --- std ---
 use std::{
+	cell::RefCell,
 	env,
 	io::{BufRead, BufReader},
 	process::{self, Command, Stdio},
@@ -27,8 +28,13 @@ use std::{
 };
 // --- crates ---
 use clap::{app_from_crate, Arg};
+#[cfg(target_os = "linux")]
+use procfs::process::Process;
 
 type PID = i32;
+
+const INIT: u8 = 0;
+const EXITED: u8 = 255;
 
 fn main() {
 	let matches = app_from_crate!()
@@ -57,16 +63,25 @@ fn main() {
 			})
 			.unwrap();
 		}
+		let status = RefCell::new(INIT);
 
 		while running.load(Ordering::SeqCst) {
-			run(script_path);
+			run(script_path, status.clone());
+
+			// TODO
+			match status.replace(INIT) {
+				EXITED => (),
+				_ => (),
+			}
+
+			thread::sleep(Duration::from_secs(3));
 		}
 
 		kill(*global::darwinia_sync_pid);
 	}
 }
 
-fn run(script_path: &str) {
+fn run(script_path: &str, status: RefCell<u8>) {
 	let (tx, rx) = mpsc::channel();
 	let mut darwinia = Command::new(script_path)
 		.stdout(Stdio::null())
@@ -85,13 +100,36 @@ fn run(script_path: &str) {
 				&mut best_number,
 				&mut idle_times,
 				|best_number, idle_times| {
-					log::trace!("Best Number: {}, Idle Times: {}", best_number, idle_times);
+					#[cfg(target_os = "linux")]
+					if let Ok(darwinia_sync_process) = Process::myself() {
+						log::trace!(
+							"Darwinia-Sync utime: {}, stime: {}, cutime: {}, cstime: {}, vsize: {}",
+							darwinia_sync_process.stat.utime,
+							darwinia_sync_process.stat.stime,
+							darwinia_sync_process.stat.cutime,
+							darwinia_sync_process.stat.cstime,
+							darwinia_sync_process.stat.vsize,
+						);
+					}
+					#[cfg(target_os = "linux")]
+					if let Ok(darwinia_process) = Process::new(darwinia_pid) {
+						log::trace!(
+							"Darwinia utime: {}, stime: {}, cutime: {}, cstime: {}, vsize: {}",
+							darwinia_process.stat.utime,
+							darwinia_process.stat.stime,
+							darwinia_process.stat.cutime,
+							darwinia_process.stat.cstime,
+							darwinia_process.stat.vsize,
+						);
+					}
+
 					log::trace!(
-						"Darwinia-Sync PID: {}, Subprocess PID: {}, Script PID: {}",
+						"Darwinia-Sync PID: {}, Darwinia-Sync TID: {}, Darwinia PID: {}",
 						*global::darwinia_sync_pid,
 						darwinia_pid,
 						darwinia_pid + 1,
 					);
+					log::trace!("Best Number: {}, Idle Times: {}", best_number, idle_times);
 				},
 			);
 
@@ -105,13 +143,13 @@ fn run(script_path: &str) {
 	});
 
 	while let Ok(received) = rx.recv() {
+		*status.borrow_mut() = received;
 		match received {
-			255 => {
+			EXITED => {
 				darwinia_thread.join().unwrap();
 				darwinia.kill().unwrap();
 				kill(darwinia_pid);
 				kill(darwinia_pid + 1);
-				thread::sleep(Duration::from_secs(3));
 
 				break;
 			}
@@ -124,7 +162,7 @@ fn sync_stalled<F>(log: &str, previous_best_number: &mut u32, idle_times: &mut u
 where
 	F: FnOnce(u32, u8),
 {
-	if let Some(captures) = global::best_number.captures(log) {
+	if let Some(captures) = global::best_number_regex.captures(log) {
 		if let Some(best_number) = captures.get(1) {
 			let best_number = best_number.as_str().parse().unwrap();
 			if *previous_best_number == best_number {
